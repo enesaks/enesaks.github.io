764eebf709f2ac806af885419f36a064e52ccf22
Enes <enes.aks29@hotmail.com>
2023-04-07 02:43:29 +0300

    test

--- mywebSite/mywebSite/urls.py
+++ mywebSite/mywebSite/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for mywebSite project.
+URL configuration for myWebsite project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -18,5 +18,6 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('enesaks.github.io/', admin.site.urls),
+    
 ]
--- mywebSite/mywebSite/wsgi.py
+++ mywebSite/mywebSite/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for mywebSite project.
+WSGI config for myWebsite project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mywebSite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myWebsite.settings')
 
 application = get_wsgi_application()
